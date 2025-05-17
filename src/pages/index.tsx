f3c8a698d97b72d55ff6390d1af915911006c10e
simple0task <simple0task@gmail.com>
2025-05-17 09:51:51 +0900

    文章修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,9 @@ const Home: NextPage = () => {
           fontWeight: 600,
           fontSize: '1.2rem',
           paddingBottom: '1rem',
-        }}>Simple Task</h1>
-        <p>複雑な作業をシステム構築や見直しでより単純な作業にして楽して</p>
-        <p>生産効率を維持できるようにサポートします。</p>
-        <p>フルスタック開発、ネットワーク構築。最近はTypscript,Nuxtで開発することが多いです。</p>
-        <p>一緒に開発しましょう。simple0task@gmail.comに連絡ください。</p>
+        }}>simpletask</h1>
+        <p>ソフトウェア開発してます</p>
+        <p>連絡先：info@simpletask.jp</p>
         <h1 style={{
           fontFamily: 'M PLUS 1p',
           fontWeight: 600,
